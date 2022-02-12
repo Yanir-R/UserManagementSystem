c6b1d9c6a6c35fa8b0976fe54e6156534a3e376c
Sheba467 <lowyouone@gmail.com>
2022-02-12 11:52:29 +0200

    switch server from local server to MongoDB & fix front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,22 +8,20 @@ import View from "./pages/View";
 import About from "./pages/About";
 import Header from "./components/Header";
 
-
-
 function App() {
   return (
-    <div className="App"> 
-    <Router>  
-      <Header />
-      <ToastContainer position="top-center" />
-    <Routes>
-            <Route path={'/'} element={<Home/>} />
-            <Route path={'/add'} element={<AddEdit/>} />
-            <Route path={'/update/:id'} element={<AddEdit/>} />
-            <Route path={'/view/:id'} element={<View/>} />
-            <Route path={'/about'} element={<About/>} />
-        </Routes>  
-    </Router>
+    <div className="App">
+      <Router>
+        <Header />
+        <ToastContainer position="top-center" />
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/add"} element={<AddEdit />} />
+          <Route path={"/update/:id"} element={<AddEdit />} />
+          <Route path={"/view/:id"} element={<View />} />
+          <Route path={"/about"} element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
